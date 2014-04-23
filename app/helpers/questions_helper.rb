e43aa3eef23c35fd4fff4287da22ba12a134d4fd
Harley Trung <harley@socialsci.com>
2014-04-23 13:34:25 +0800

    more cleaning up

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -6,13 +6,17 @@ module QuestionsHelper
   def render_question_choices question
     content_for :div, class: 'question-choices form-horizontal' do
       question.choices.each do |choice|
-        render_choice choice
+        render_choice(choice)
       end
     end
   end
 
   def render_question_title question
-    safe_join [content_tag(:span, question.kind, class: 'label label-default'), ' ', content_tag(:strong, question.title)]
+    if admin?
+      safe_join [content_tag(:span, question.kind, class: 'label label-default'), ' ', content_tag(:strong, question.title)]
+    else
+      content_tag :strong, question.title
+    end
   end
 
   def add_choice_link(question)
